cbff31929abdea21ce22c66840a87551865a7bbe
Mitesh singh <102028317+Mitesh957@users.noreply.github.com>
2024-03-19 23:07:00 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,18 +1,3 @@
-
-   const firebaseConfig = {
-    apiKey: "AIzaSyCqTWhstnYIMAWxFKlFmhqV5kXfDsMwm7Q",
-    authDomain: "work-vibe.firebaseapp.com",
-    projectId: "work-vibe",
-    storageBucket: "work-vibe.appspot.com",
-    messagingSenderId: "657553447517",
-    appId: "1:657553447517:web:b31d13b5e033c32d7a8bb4",
-    measurementId: "G-HEN3V080PD",
-    databaseURL: "https://work-vibe-default-rtdb.firebaseio.com/" // Add your Firebase Realtime Database URL here
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 // Reference your database
 const contactFormDB = firebase.database().ref("contactForm");
 
